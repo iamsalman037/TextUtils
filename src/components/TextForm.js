bfec3c8fd7636c5a6e7eaf534cf7fb9aae53c238
iamsalman037 <syedsalman225@gmail.com>
2022-07-08 03:58:12 +0530

    Added TextUtils

---
+++ src/components/TextForm.js
@@ -0,0 +1,40 @@
+import React,{useState} from 'react'
+
+export default function TextForm(props) {
+    const[text,setText] = useState("");
+    const handleUpClick = ()=>{
+        let newText = text.toUpperCase();
+        setText(newText);
+    }
+    const handleLowClick = ()=>{
+        let newText = text.toLowerCase();
+        setText(newText);
+    }
+    const handleOnChange = (event)=>{
+        setText(event.target.value);
+    }
+    const handleClear =()=>{
+        setText("");
+    }
+  return (
+    <>
+    <div className="container my-5">
+        <h1>{props.heading}</h1>
+        <div className="mb-3 my-3">
+            <textarea className="form-control" id="myBox" value= {text} onChange={handleOnChange} rows="8"></textarea>
+        </div>
+        <button className="btn btn-primary my-3" onClick={handleUpClick}>Convert to Upper case</button>
+        <button className="btn btn-primary mx-3 my-3" onClick={handleLowClick}>Convert to Lower case</button>
+        <button className="btn btn-primary my-3" onClick={handleClear}>Clear Text</button>
+    </div>
+    <div className="container my-3">
+        <h2>Your Text Summary</h2>
+        <p>{text.split(" ").length} words and {text.length} characters</p>
+        <p> {0.008 * text.split(" ").length} minutes to read</p>
+        <h2>Preview</h2>
+        <p>{text}</p>
+    </div>
+    </>
+    
+  )
+}

a213138a6b37d37988136b9308ade454fc6fb2f8
iamsalman037 <syedsalman225@gmail.com>
2022-07-09 04:10:15 +0530

    Added Alert component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
 import { useState } from 'react';
 import './App.css';
+import Alert from './components/Alert';
 //import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm'
 
 function App() {
   const[mode,setMode]=useState('light');
+  const[alert,setAlert]=useState(null);
+  const showAlert =(message,type)=>{
+      setAlert({
+        msg: message,
+        type:type
+      })
+      setTimeout(() => {
+          setAlert(null);
+      }, 1500);
+  }
   const toggleMode =()=>{
     if(mode==='light'){
       setMode('dark');
       document.body.style.backgroundColor = '#042743';
+      showAlert("Dark Mode has been enabled","success");
     }
     else{
       setMode('light');
       document.body.style.backgroundColor = 'white';
+      showAlert("Light Mode has been enabled","success");
     }
   }
   return (
     <>
-    <Navbar title="TextUtils" aboutText = "About" mode={mode} toggleMode={toggleMode}/>
+    <Navbar title="TextUtils" aboutText = "About" mode={mode} alert={alert} toggleMode={toggleMode}/>
+    <Alert alert={alert}/>
     <div className="container">
-       <TextForm heading= "Enter the text to analyze below" mode={mode}/>
+       <TextForm heading= "Enter the text to analyze below" mode={mode} showAlert={showAlert}/>
+       
       {/* <About/> */}
     </div>
     </>    
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -5,16 +5,19 @@ export default function TextForm(props) {
     const handleUpClick = ()=>{
         let newText = text.toUpperCase();
         setText(newText);
+        props.showAlert("Converted to Upper case","success");
     }
     const handleLowClick = ()=>{
         let newText = text.toLowerCase();
         setText(newText);
+        props.showAlert("Converted to Lower case","success");
     }
     const handleOnChange = (event)=>{
         setText(event.target.value);
     }
     const handleClear =()=>{
         setText("");
+        props.showAlert("Text has been cleared","success");
     }
   return (
     <>
